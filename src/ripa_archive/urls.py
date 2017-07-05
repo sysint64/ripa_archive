83d55db9dc7552e51000c484d90f8fc626979951
Andrey Kabylin <andrey@kabylin.ru>
2017-07-06 00:57:25 +0700

    ansible config

--- src/ripa_archive/urls.py
+++ src/ripa_archive/urls.py
@@ -2,10 +2,17 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 from ripa_archive import views
 
+
+def redirect_to_browser(request):
+    return redirect("documents:browser")
+
+
 urlpatterns = [
+    url(r'^$', redirect_to_browser),
     url(r'^documents/', include('ripa_archive.documents.urls', namespace="documents")),
     url(r'^accounts/', include('ripa_archive.accounts.urls', namespace="accounts"))
 ]
