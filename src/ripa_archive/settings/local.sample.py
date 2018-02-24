df1c0b8729d34880bea90b8c2fce8bc2a15b1643
Andrey Kabylin <andrey@kabylin.ru>
2018-02-25 00:34:50 +0700

    update local.sample.py

--- src/ripa_archive/settings/local.sample.py
+++ src/ripa_archive/settings/local.sample.py
@@ -30,5 +30,38 @@ LOCALE_PATHS = (
 )
 
 COMPRESS_PRECOMPILERS = (
-    ('text/x-sass', "sassc"+' --style compressed {infile} {outfile}'),
+    ('text/x-sass', "sassc --style compressed {infile} {outfile}"),
+    ('text/x-scss', "sassc --style compressed {infile} {outfile}"),
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(FRONTEND_ROOT, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'ripa_archive.context_processors.search',
+                'ripa_archive.context_processors.common',
+            ],
+        },
+    },
+]
+
+STATICFILES_DIRS = (
+    os.path.join(FRONTEND_ROOT, 'static'),
+)
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://localhost:9001/solr/ripa_archive',
+        'INCLUDE_SPELLING': True,
+    },
+}
